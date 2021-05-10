b2f2c5b7ff1c333ee01b27fc799c3580aec70cc8
techy3000 <will.andrews98@yahoo.co.uk>
2021-05-10 13:36:53 +0100

    Add

---
+++ Dissertation Project/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dissertation_Project
+{
+    public class ChatHub : Hub
+    {
+
+        public async Task Send(string name, string message)
+        {
+
+           await Clients.All.SendAsync("ReceiveMessage", name, message);
+
+        }
+
+    }
+}
