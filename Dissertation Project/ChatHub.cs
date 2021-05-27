db9f0be0295d134f1853f6da4e4c2659c60b60c6
techy3000 <will.andrews98@yahoo.co.uk>
2021-05-27 22:32:47 +0100

    Groups not working

--- Dissertation Project/ChatHub.cs
+++ Dissertation Project/ChatHub.cs
@@ -8,9 +8,9 @@ namespace Dissertation_Project
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string user, string message)
+        public async Task SendMessage(string groupName, string user, string message)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Clients.Group(groupName).SendAsync("ReceiveMessage", user, message);
         }
 
         public async Task SendTask(string message)
@@ -21,6 +21,28 @@ namespace Dissertation_Project
         public async Task CompTask(string user, string type)
         {
             await Clients.All.SendAsync("ComplateTask", user, type);
+        }
+
+        public async Task AddToGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+            Console.WriteLine($"{Context.ConnectionId} added to {groupName}");
+        }
+
+        public async Task RemoveToGroup(string groupName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+            Console.WriteLine($"{Context.ConnectionId} removed to {groupName}");
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception e)
+        {
+            await base.OnDisconnectedAsync(e);
         }
 
     }
